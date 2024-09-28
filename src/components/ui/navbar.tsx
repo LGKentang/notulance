6a5017be20bb16aa20bc0940bf93985fd089e09d
Wilmer Kwodee <152074108+Wilmer-Kwodee@users.noreply.github.com>
2024-09-28 14:29:54 +0700

    FE Homepage done (minor bug still present)

--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -1,23 +1,22 @@
 import * as React from "react"
 
-import { cn } from "@/lib/utils"
-
 export interface InputProps
   extends React.InputHTMLAttributes<HTMLInputElement> {}
 
 const NavBar = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, ...props }, ref) => {
     return (
-        <div className={cn("flex p-7 items-center justify-between")}>
-            <div id="left">
-                <a href="" className={cn("text-black")}>Notulance</a>
+        <div className="flex p-7 items-center justify-between bg-white drop-shadow-2xl">
+            <div id="left" className="flex">
+                <img src="/public/notulance.png" className="w-10 mx-5"/>
+                <a href="" className="text-black text-3xl">Notulance</a>
             </div>
-            <div id="right" className={cn("w-1/4 flex items-center justify-between")} 
+            <div id="right" className="w-1/4 flex items-center justify-between" 
             >
-                <a href="" className={cn("text-black")}>Search</a>
-                <a href="" className={cn("text-black")}>Sell</a>
-                <a href="/login" className={cn("text-black")}>
-                    <button className={cn("text-white bg-red-500")} >Sign In</button>
+                <a href="" className="text-black">Search</a>
+                <a href="" className="text-black">Sell</a>
+                <a href="/login" className="text-black">
+                    <button className="text-white bg-red-500 mr-20" >Sign In</button>
                 </a>
             </div>
         </div>
