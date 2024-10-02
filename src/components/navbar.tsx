420cc8947da050eea73236583e5169967c455a7c
Wilmer Kwodee <152074108+Wilmer-Kwodee@users.noreply.github.com>
2024-10-02 20:01:26 +0700

    Cart Finally Completed

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -18,27 +18,45 @@ import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { Avatar } from "./ui/avatar";
 import { AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
 import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
 import {
   Popover,
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover"
 import CartChild from "./cart-child";
+import { getCartById } from "@/api/cart-api";
+import { getUserByAuthId } from "@/api/user-api";
+import { FirebaseUser } from "@/interfaces/user/firebase-user";
+import { checkoutCart } from "@/handlers/cart-handler";
 
 const NavBar = () => {
   const [authenticated, setAuthenticated] = useState<boolean>(false);
+  const [cart, setCart] = useState<any | null>([])
+
   useEffect(() => {
     const auth = getAuth();
     
-    const unsubscribe = onAuthStateChanged(auth, (user) => {
+    const unsubscribe = onAuthStateChanged(auth, async (user) => {
       if (user) {
         setAuthenticated(true);
-      } else {
+
+        const userId = user.uid;
+        let userObject = await getUserByAuthId(userId) as FirebaseUser | null
+        if (userObject == null) throw new Error("User is not found");
+        const cartId = userObject.cartId
+        const theCart = await getCartById(cartId)
+
+        if(theCart != null){
+          const cartItems = theCart.items
+          setCart(cartItems)
+          console.log(cartItems)
+        }
+      } 
+      else {
         setAuthenticated(false);
       }
     });
+
 
     return () => unsubscribe();
   }, []);
@@ -58,90 +76,66 @@ const NavBar = () => {
           <div id="right" className="flex items-center space-x-16">
               <a href="/note/search"><Button variant="link">Search</Button></a>
               <a href="/note/sell"><Button variant="link">Sell</Button></a>
-              <Popover>
-                <PopoverTrigger asChild>
-                  <Button variant="outline" className="w-14 rounded-3xl">
-                    <img src="https://cdn-icons-png.flaticon.com/512/1413/1413908.png"></img>
-                  </Button>
-                </PopoverTrigger>
-                <PopoverContent className="w-80 z-50 bg-white">
-                  <div className="grid gap-4">
-                    <div className="space-y-2">
-                      <h4 className="font-medium leading-none">My Shopping Cart</h4>
-                      <p className="text-sm text-muted-foreground">
-                        Your added notes in search page will be shown here:
-                      </p>
-                    </div>
-                    <div className="grid gap-2">
-
-                      <CartChild title="Introduction to Automata Theory 2"/>
-                      
-                    </div>
-                    <div>
-                      <span>Total price: Rp37,500</span>
-                    </div>
-                    <div className="flex justify-between">
-                      <Button className="w-24 bg-red-400 rounded-3xl">Clear Cart</Button>
-                      <Button className="w-24 bg-green-400 rounded-3xl">Checkout!</Button>
-                    </div>
-                  </div>
-                </PopoverContent>
-              </Popover>
-              
-              {/* <DropdownMenu>
-                  <DropdownMenuTrigger asChild>
-                    <Button variant="ghost" className="p-0">
-                      <Avatar>
-                        <AvatarImage src="https://cdn-icons-png.flaticon.com/512/1413/1413908.png" />
-                        <AvatarFallback>N</AvatarFallback>
-                      </Avatar>
-                    </Button>
-                  </DropdownMenuTrigger>
-                  <DropdownMenuContent className="w-56">
-                    <DropdownMenuLabel>Cart Items:</DropdownMenuLabel>
-                    <DropdownMenuSeparator />
-                    <DropdownMenuGroup>
-                      <DropdownMenuItem>
-                        Profile
-                      </DropdownMenuItem>
-                      <DropdownMenuItem>
-                        Seller Profile
-                      </DropdownMenuItem>
-                    </DropdownMenuGroup>
-                    <DropdownMenuSeparator />
-                    <DropdownMenuItem className="text-red-500">
-                      Empty Cart
-                    </DropdownMenuItem>
-                  </DropdownMenuContent>
-                </DropdownMenu> */}
 
               {authenticated ? (
-                <DropdownMenu>
-                  <DropdownMenuTrigger asChild>
-                    <Button variant="ghost" className="p-0">
-                      <Avatar>
-                        <AvatarImage src="https://github.com/shadcn.png" />
-                        <AvatarFallback>N</AvatarFallback>
-                      </Avatar>
-                    </Button>
-                  </DropdownMenuTrigger>
-                  <DropdownMenuContent className="w-56">
-                    <DropdownMenuLabel>Name</DropdownMenuLabel>
-                    <DropdownMenuSeparator />
-                    <DropdownMenuGroup>
-                      <DropdownMenuItem>
-                        Profile
+                <>
+                  <Popover>
+                    <PopoverTrigger asChild>
+                      <Button variant="outline" className="w-14 rounded-3xl">
+                        <img src="https://cdn-icons-png.flaticon.com/512/1413/1413908.png"></img>
+                      </Button>
+                    </PopoverTrigger>
+                    <PopoverContent className="w-80 z-50 bg-white">
+                      <div className="grid gap-4">
+                        <div className="space-y-2">
+                          <h4 className="font-medium leading-none">My Shopping Cart</h4>
+                          <p className="text-sm text-muted-foreground">
+                            Your added notes in search page will be shown here:
+                          </p>
+                        </div>
+                        <div className="grid gap-2">
+                          {cart.map((x: any, index: number) => 
+                            <CartChild title={x.item.title} index={index} key={index}/>
+                          )}
+                        </div>
+                        <div>
+                          <span>Total price: Rp37,500</span>
+                        </div>
+                        <div className="flex justify-between">
+                          <Button className="w-24 bg-red-400 rounded-3xl">Clear Cart</Button>
+                          <Button className="w-24 bg-green-400 rounded-3xl" onClick={checkoutCart}>Checkout!</Button>
+                        </div>
+                      </div>
+                    </PopoverContent>
+                  </Popover>
+                  <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                      <Button variant="ghost" className="p-0">
+                        <Avatar>
+                          <AvatarImage src="https://github.com/shadcn.png" />
+                          <AvatarFallback>N</AvatarFallback>
+                        </Avatar>
+                      </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent className="w-56">
+                      <DropdownMenuLabel>Name</DropdownMenuLabel>
+                      <DropdownMenuSeparator />
+                      <DropdownMenuGroup>
+                        <DropdownMenuItem>
+                          Profile
+                        </DropdownMenuItem>
+                        <DropdownMenuItem>
+                          Seller Profile
+                        </DropdownMenuItem>
+                      </DropdownMenuGroup>
+                      <DropdownMenuSeparator />
+                      <DropdownMenuItem className="text-red-500">
+                        Log out
                       </DropdownMenuItem>
-                      <DropdownMenuItem>
-                        Seller Profile
-                      </DropdownMenuItem>
-                    </DropdownMenuGroup>
-                    <DropdownMenuSeparator />
-                    <DropdownMenuItem className="text-red-500">
-                      Log out
-                    </DropdownMenuItem>
-                  </DropdownMenuContent>
-                </DropdownMenu>
+                    </DropdownMenuContent>
+                  </DropdownMenu>
+                </>
+                
               ) : (
                 <a href="/login"><Button variant="default">Sign In</Button></a>
               )}
--- src/pages/note-details.tsx
+++ src/pages/note-details.tsx
@@ -1,9 +1,17 @@
 import { getNoteById } from '@/api/note-api';
 import { NavBar } from '@/components/navbar';
+import { Button } from '@/components/ui/button';
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { addItemToCart } from '@/api/cart-api';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { CartItem } from '@/interfaces/transaction/cart';
+import { Note } from '@/interfaces/general/note';
+import { addCartItemToCart } from '@/handlers/cart-handler';
 
 const NoteDetails = () => {
+    const [userId, setUserId] = useState<any | null>(null);
+
     const { noteId } = useParams();
     const [note, setNote] = useState<any | null>(null);
 
@@ -19,16 +27,56 @@ const NoteDetails = () => {
                 console.error("Error fetching note:", error);
             }
         };
-
         fetchNote();
+
+        const auth = getAuth();
+        const unsubscribe = onAuthStateChanged(auth, (user) => {
+          if (user) {
+            setUserId(user.uid);
+          } else {
+            setUserId(null);
+          }
+        });
     }, [noteId]);
+
+    async function handleAddToCart(){
+        const uid = userId
+        const noteObject: Note = {
+            id: note.id,
+            title: note.title,
+            description: note.description,
+            writerId: note.writerId,
+            price: note.price,
+            subject: note.subject,
+            releaseDate: note.releaseDate,
+            university: note.university,
+            grade: note.grade,
+            fileId: note.fileId,
+            score: note.score,
+            bundleId: note.bundleId,
+            thumbnailUrl: note.thumbnailUrl,
+            totalPages: note.totalPages,
+            ranking: note.ranking,
+        }
+        const cartItem: CartItem = {
+            type: 'note',
+            item: noteObject   
+        }
+        console.log(cartItem)
+
+        await addCartItemToCart(cartItem);
+        window.location.reload()
+    }
 
     return (
         <div className="w-screen h-screen flex flex-col font-itim">
             <NavBar />
             {note ? (
-                <div className='py-5 px-20 flex'>
+                <div className='py-5 px-20 '>
+                    <h3>
                     {note.title}
+                    </h3>
+                    <Button onClick={handleAddToCart} className='bg-red-500'>Add to Cart</Button>
                 </div>
             ) : (
                 <div className='w-full h-full flex justify-center items-center'>
